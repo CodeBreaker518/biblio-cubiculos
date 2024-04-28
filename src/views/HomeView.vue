82e42e000e6616df8ebe4e9767860bc760be8230
Diego Diaz Segovia <diegodiazsegovia02@gmail.com>
2024-04-27 22:30:28 -0600

    Resolucion de errores

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,27 @@
 <template>
-    <div class="page-container">
+  <div class="page-container">
     <div>
       <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
         <div class="container-fluid"> 
           <div class="navbar-brand me-2">
             <img
               src="@/assets/logo2.png"
-              height="3"
+              height="30"
               loading="lazy"
-              style="margin-top: -1px;"
+              alt="Logo"
             />
           </div>
           <div class="collapse navbar-collapse justify-content-end" id="navbarButtonsExample">
             <ul class="navbar-nav mb-2 mb-lg-0">
             </ul>
-            <div id="Bottons" class="d-flex align-items-center">
+            <div id="Buttons" class="d-flex align-items-center">
               <router-link to="/login" class="btn btn-link px-3 me-2">Iniciar Sesión</router-link>
-              <router-link to="/Register" class="btn btn-primary me-3 rounded-pill">Registrarse</router-link>
+              <router-link to="/register" class="btn btn-primary me-3 rounded-pill">Registrarse</router-link>
             </div>
           </div>
         </div>
       </nav>
-  
+
       <div class="image-container">
         <transition name="fade">
           <img :key="currentIndex" :src="currentImage" :alt="'Image ' + (currentIndex + 1)" class="image">
@@ -77,53 +77,51 @@ export default {
   }
 };
 </script>
-  
-  <style scoped>
-  .navbar {
-    background-color: #f6f6f6;
-    padding-top: 5px;
-    padding-bottom: 5px;
-  }
-  .navbar-brand img {
-    height: 125px;
-    position: relative;
-    top: 15px;
-    left: -35vw;
-  }
-  .btn.btn-primary {
-    padding: 10px 20px;
-    background-color: #c9a400; 
-    border-color: #c9a400; 
-    color: white; 
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-  }
-  .btn.btn-link{
-    padding: 10px 20px;
-  }
-  .image-container {
-    height: 735px; 
-  }
-  .image-container img {
-    width: 100%; 
-    height: 100%; 
-    display: block; 
-    margin-bottom: 10px; 
-    object-fit: cover; 
-  }
-  .fade-enter-active, .fade-leave-active {
-    transition: opacity 1s;
-  }
-  .fade-enter, .fade-leave-to {
-    opacity: 0;
-  }
-  #Bottons{
-    position: relative;
-    top: -75px;
-    left: 40vw;
-  }
-  .page-container {
+
+<style scoped>
+.navbar {
+  background-color: #f6f6f6;
+  padding-top: 5px;
+  padding-bottom: 5px;
+}
+.navbar-brand img {
+  height: 150px;
+}
+.btn.btn-primary {
+  padding: 10px 20px;
+  background-color: #c9a400; 
+  border-color: #c9a400; 
+  color: white; 
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+.btn.btn-link {
+  padding: 10px 20px;
+}
+.image-container {
+  height: 755px; 
+  position: relative; 
+}
+.image-container img {
+  width: 100%; 
+  height: 100%; 
+  display: block; 
+  object-fit: cover; 
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 1s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+#Buttons {
+  position: absolute; 
+  bottom: 20px; 
+  right: 20px;
+  top: 25px; 
+}
+.page-container {
   overflow: hidden;
 }
 </style>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -13,55 +13,50 @@
             <div id="radius-shape-2" class="position-absolute shadow-5-strong"></div>
             <div class="card bg-glass">
               <div class="card-body px-4 py-5 px-md-5">
-
-                <form>
-                  
-                  <div class="row">
-                    <h1 class="my-5 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
-                      <img src="@/assets/logo.png" class="icon"/>
-                    </h1>
-                    <div class="col-md-6 mb-4">
+                <form @submit.prevent="submitForm">
+                  <h1 class="my-5 display-5 fw-bold ls-tight" style="color: hsl(218, 81%, 95%)">
+                    <img src="@/assets/logo.png" class="icon"/>
+                  </h1>
+                  <div class="row mb-4">
+                    <div class="col-md-6">
                       <div data-mdb-input-init class="form-outline">
                         <input type="text" id="nombre" class="form-control"/>
-                        <label class="form-label" for="form3Example1">Nombre</label>
+                        <label class="form-label" for="nombre">Nombre</label>
                       </div>
                     </div>
-                    <div class="col-md-6 mb-4">
+                    <div class="col-md-6">
                       <div data-mdb-input-init class="form-outline">
                         <input type="text" id="apellido" class="form-control"/>
-                        <label class="form-label" for="form3Example2">Apellido</label>
+                        <label class="form-label" for="apellido">Apellido</label>
                       </div>
                     </div>
                   </div>
-
-                  <div data-mdb-input-init class="form-outline mb-4">
-                    <input type="email" id="correo" class="form-control"/>
-                    <label class="form-label" for="form3Example3">Correo</label>
+                  <div class="row mb-4">
+                    <div class="col-md-6">
+                      <div data-mdb-input-init class="form-outline">
+                        <input type="email" id="correo" class="form-control"/>
+                        <label class="form-label" for="correo">Correo</label>
+                      </div>
+                    </div>
+                    <div class="col-md-6">
+                      <div data-mdb-input-init class="form-outline">
+                        <input type="password" id="contraseña" class="form-control"/>
+                        <label class="form-label" for="contraseña">Contraseña</label>
+                      </div>
+                    </div>
                   </div>
-                  
-                  <div data-mdb-input-init class="form-outline mb-4">
-                    <input type="password" id="contraseña" class="form-control"/>
-                    <label class="form-label" for="form3Example4">Contraseña</label>
-                  </div>
-                  
-                  <div class="row">
-                    <div class="col-md-6 mb-4">
+                  <div class="row mb-4">
+                    <div class="col-md-6">
                       <button :class="{ 'btn-primary': rolSeleccionado === 'estudiante', 'btn-secondary': rolSeleccionado === 'administrador' }" type="button" class="btn" @click="seleccionarRol('estudiante')">Estudiante</button>
                     </div>
-                    <div class="col-md-6 mb-4">
+                    <div class="col-md-6">
                       <button :class="{ 'btn-primary': rolSeleccionado === 'administrador', 'btn-secondary': rolSeleccionado === 'estudiante' }" type="button" class="btn" @click="seleccionarRol('administrador')">Administrador</button>
                     </div>
                   </div>
-                  
-                  <div v-if="rolSeleccionado === 'estudiante'" class="form-outline mb-4">
-                    <input type="text" id="nua" class="form-control"/>
-                    <label class="form-label" for="nua">NUA</label>
+                  <div v-if="rolSeleccionado === 'estudiante' || rolSeleccionado === 'administrador'" class="form-outline mb-4">
+                    <input type="text" :id="rolSeleccionado === 'estudiante' ? 'nua' : 'nue'" class="form-control"/>
+                    <label class="form-label" :for="rolSeleccionado === 'estudiante' ? 'nua' : 'nue'">{{ rolSeleccionado === 'estudiante' ? 'NUA' : 'NUE' }}</label>
                   </div>
-                  <div v-if="rolSeleccionado === 'administrador'" class="form-outline mb-4">
-                    <input type="text" id="nue" class="form-control"/>
-                    <label class="form-label" for="nue">NUE</label>
-                  </div>
-                  
                   <button id="Registro" type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary">
                     Registrarse
                   </button>
@@ -85,34 +80,31 @@ export default {
   },
   methods: {
     seleccionarRol(rol) {
-      if (this.rolSeleccionado !== rol) {
-        this.rolSeleccionado = rol;
-      } else {
-        this.rolSeleccionado = null;
-      }
+      this.rolSeleccionado = rol;
+    },
+    submitForm() {
+      console.log('Formulario enviado');
     }
   }
 }
 </script>
 
-
 <style scoped>
-
 .cuerpo {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: fixed;
-    top: 0;
-    right: -125px;
-    width: 100%;
-    height: 100vh; 
-    background-color: #fdf7d2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100vh; 
+  background-color: #fdf7d2;
 }
 
 .bg-glass {
-      background-color: hsla(0, 0%, 100%, 0.9) !important;
-      backdrop-filter: saturate(200%) blur(25px);
+  background-color: hsla(0, 0%, 100%, 0.9) !important;
+  backdrop-filter: saturate(200%) blur(25px);
 }
 
 .btn {
@@ -134,12 +126,12 @@ export default {
 }
 
 .icon {
-    position: relative;
-    width: 100%; 
-    height: auto; 
-    right: 0px;
-    max-width: 300px;
-    max-height: 300px;
+  position: relative;
+  width: 100%; 
+  height: auto; 
+  right: -80px;
+  max-width: 300px; /* Cambiar esto por max-width: 100%; */
+  max-height: 300px;
 }
 
 .rectangulo-purpura {
@@ -153,8 +145,7 @@ export default {
 
 .card.bg-glass {
   position: relative;
-  width: 100%;
-  max-width: 1000vw; 
+  width: 30vw;
   margin: auto;
   top: 25px;
   left: 15vw;
@@ -169,8 +160,7 @@ export default {
 }
 
 #nombre, #apellido, #correo, #contraseña, #nua, #nue {
-  margin: 15px;
+  margin-bottom: 15px; 
   padding: 2px;
 }
-
 </style>
