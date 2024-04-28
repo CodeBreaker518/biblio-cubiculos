2ab5133af10ac0c4972de4322b549773772e7f45
Diego Diaz Segovia <diegodiazsegovia02@gmail.com>
2024-04-27 18:36:49 -0600

    Correcion de diseño y esqueleto de interfaz usuario

--- src/main.js
+++ src/main.js
@@ -2,11 +2,4 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap'
-
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App).use(router).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import RegisterView from '../views/RegisterView.vue'
+import VistaInicio from '../views/VistaInicio.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Inicio',
+    component: VistaInicio
+  },
+  {
+    path: '/Register',
+    name: 'Register',
+    component: () => import('../views/RegisterView.vue')
+  },
+  {
+    path: '/InterUsuario',
+    name: 'InterUsuario',
+    component: () => import('../views/InterUsuarioView.vue')
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: RegisterView
-    }
-  ]
+  history: createWebHistory(process.env.BASE_URL),
+  routes
 })
 
 export default router
