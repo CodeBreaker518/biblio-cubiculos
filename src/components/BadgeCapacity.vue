e8edc4fa75a585591b7a9df79c2b90fcfa73a0dd
Diego Perez Perez <darksilent518@gmail.com>
2024-05-25 23:42:17 -0600

    fix: styling to BadgeCapacity component

--- src/components/BadgeCapacity.vue
+++ src/components/BadgeCapacity.vue
@@ -18,19 +18,17 @@ export default {
   display: inline-block;
   padding: 0.25em 0.4em;
   font-size: 14px;
-  font-weight: 500;
+  font-weight: 600;
   line-height: 1;
   text-align: center;
   white-space: nowrap;
   vertical-align: baseline;
   border-radius: 0.25rem;
 }
-
 .badge-capacity {
   color: #fff;
   background-color: rgb(122, 122, 247);
 }
-
 .badge-danger {
   color: #fff;
   background-color: #dc3545;
