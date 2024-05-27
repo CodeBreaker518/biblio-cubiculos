eae0e92d5466ed3bbe51d53fbdabfad8f28fdd19
VidarSD <gutierrezcardenasjesus@gmail.com>
2024-05-27 04:23:42 -0600

    AdminTasks

---
+++ src/components/BadgeReservaStatus.vue
@@ -0,0 +1,37 @@
+<template>
+    <span :class="['badgeCapacity', 'badge-capacity']"> Estado  : {{ capacity }} </span>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      capacity: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .badgeCapacity {
+    display: inline-block;
+    padding: 0.25em 0.4em;
+    font-size: 14px;
+    font-weight: 600;
+    line-height: 1;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: baseline;
+    border-radius: 0.25rem;
+  }
+  .badge-capacity {
+    color: #fff;
+    background-color: rgb(122, 122, 247);
+  }
+  .badge-danger {
+    color: #fff;
+    background-color: #dc3545;
+  }
+  </style>
+  
--- src/views/ReservarCubiculosUserView.vue
+++ src/views/ReservarCubiculosUserView.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="reserva-cubiculo">
       <h1>Reservar Cubículo</h1>
-      <form @submit.prevent="handleSubmit">
+      <form @submit.prevent="crearReserva">
         
         <div class="mb-3">
           <label for="cubiculo" class="form-label">Cubículo</label>
-          <select class="form-select" v-model="cubiculo" required>
+          <select class="form-select" v-model="selected_cub" required>
             <option v-for="cub in cubiculos" :key="cub.id" :value="cub.id">
-              {{ cub.name }}
+              {{ cub.cub_nombre }}
             </option>
           </select>
         </div>
@@ -32,35 +32,119 @@
     </div>
   </template>
   
-  <script>
-  export default {
-    data() {
-      return {
-        cubiculo: '',
-        fecha: '',
-        horaInicio: '',
-        horaFin: '',
-        cubiculos: [
-          { id: 1, name: 'Cubículo 1' },
-          { id: 2, name: 'Cubículo 2' },
-          { id: 3, name: 'Cubículo 3' }
-        ]
-      };
-    },
-    methods: {
-      handleSubmit() {
-        const reserva = {
-          usuario: this.usuario,
-          cubiculo: this.cubiculo,
-          fecha: this.fecha,
-          horaInicio: this.horaInicio,
-          horaFin: this.horaFin
-        };
-        console.log('Reserva realizada:', reserva);
-        // Aquí puedes realizar una solicitud a la API para guardar la reserva
-      }
-    }
+  <script setup>
+  import { getAuth,onAuthStateChanged } from "firebase/auth";
+  import { getFirestore, collection,onSnapshot,addDoc } from 'firebase/firestore';
+  import { ref } from 'vue';
+  import { useRouter } from "vue-router";
+
+  const router = useRouter();
+
+
+  const auth = getAuth();
+  var usuario = ref("");
+  
+  const fecha = ref("");
+  const horaInicio = ref("");
+  const horaFin = ref("");
+  
+//const status = ref("open");
+
+ onAuthStateChanged(auth,  (user) => {
+  
+  
+   if (user) {
+
+     usuario= user.uid;
+     console.log(usuario);
+
+ } else {
+     console.log("No existe usuario loggeado");
+      router.push("/");
+   }
+});
+
+
+
+  // query, where, getDocs
+  const db = getFirestore();
+
+  const cubiculos = ref([]);
+  const selected_cub = ref (""); 
+
+  
+  const cargarCubiculos = () => {
+  const cubiculosCollection = collection(db, "cubiculo");
+  onSnapshot(cubiculosCollection, (snapshot) => {
+    cubiculos.value = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    console.log (cubiculos.value)
+  });
   };
+
+  cargarCubiculos ();
+
+  
+
+const crearReserva = async () =>
+{
+  if ( selected_cub.value && fecha.value && horaFin.value && horaFin.value)
+  {
+      await addDoc(collection (db,"reserva"), 
+      {
+        r_usuario : usuario,
+        r_cubiculo : selected_cub.value,
+        r_fecha : fecha.value,
+        r_hora_inicio : horaFin.value,
+        r_hora_fin : horaInicio.value,
+        r_username : localStorage.getItem("username"),
+        r_status : "open"
+
+        
+      });
+      alert("Reserva Creada con exito")
+      console.log ("Reserva Creada ");
+      router.push('/InterUsuario');
+      
+  } else
+  {
+    alert("Complete todos los campos para continuar");
+  }
+
+}
+
+console.log(localStorage.getItem("username"))
+
+  // export default {
+  //   data() {
+  //     return {
+  //       cubiculo: '',
+  //       fecha: '',
+  //       horaInicio: '',
+  //       horaFin: '',
+  //       cubiculos: [
+  //         { id: 1, name: 'Cubículo 1' },
+  //         { id: 2, name: 'Cubículo 2' },
+  //         { id: 3, name: 'Cubículo 3' },
+  //         { id: 4, name: 'Cubículo 4' }
+  //       ]
+  //     };
+  //   },
+  //   methods: {
+  //     handleSubmit() {
+  //       const reserva = {
+  //         usuario: this.usuario,
+  //         cubiculo: this.cubiculo,
+  //         fecha: this.fecha,
+  //         horaInicio: this.horaInicio,
+  //         horaFin: this.horaFin
+  //       };
+  //       console.log('Reserva realizada:', reserva);
+  //       // Aquí puedes realizar una solicitud a la API para guardar la reserva
+  //     }
+  //   }
+  //};
+
+
   </script>
   
   <style scoped>
